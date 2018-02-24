fd662e4b642ba9ba207eace00258250e3aff22d9
Dan Pock <dpock32509@gmail.com>
2018-02-24 04:06:16 -0500

    Update exception and add docblock comments

--- src/Client.php
+++ src/Client.php
@@ -3,21 +3,47 @@ namespace MallardDuck\Whois;
 
 use Hoa\Socket\Client as SocketClient;
 use MallardDuck\Whois\WhoisServerList\Locator;
+use MallardDuck\Whois\Exceptions\MissingArgException;
 
+/**
+ * The Whois Client Class.
+ *
+ * @author mallardduck <dpock32509@gmail.com>
+ * @copyright lucidinternets.com 2018
+ * @version 1.0.0
+ */
 class Client
 {
+
+    /**
+     * The TLD Whois locator class.
+     * @var Locator
+     */
     private $tldLocator;
+
+    /**
+     * The carriage return line feed character comobo.
+     * @var string
+     */
     private $clrf = "\r\n";
 
+    /**
+     * Construct the Whois Client Class.
+     */
     public function __construct()
     {
         $this->tldLocator = new Locator;
     }
 
+    /**
+     * Performs a Whois look up on the domain provided.
+     * @param  string $domain The domain being looked up via whois.
+     * @return string         The output of the Whois look up.
+     */
     public function lookup($domain = '')
     {
         if (empty($domain)) {
-            throw new \Exception("Must enter domain name.");
+            throw new MissingArgException("Must provide a domain name when using lookup method.");
         }
         // Get the domains whois server.
         $whoisServer = $this->tldLocator->getWhoisServer($domain);
