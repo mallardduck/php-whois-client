10c2bf74764e77b5790aa535eedbf586b81ce83b
Dan Pock <dpock32509@gmail.com>
2018-02-24 04:07:21 -0500

    Add first test for client

---
+++ tests/WhoisClientTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace MallardDuck\Whois\Test;
+
+use MallardDuck\Whois\Client;
+use PHPUnit\Framework\TestCase;
+use MallardDuck\Whois\Exceptions\MissingArgException;
+
+/**
+*  Corresponding Class to test the whois Client class
+*
+*  For each class in your library, there should be a corresponding Unit-Test for it
+*  Unit-Tests should be as much as possible independent from other test going on.
+*
+* @author mallardduck <dpock32509@gmail.com>
+*/
+class WhoisClientTest extends TestCase
+{
+
+  /**
+  * Just check if the YourClass has no syntax error
+  *
+  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+  * any typo before you even use this library in a real project.
+  *
+  */
+    public function test_is_there_any_syntax_error()
+    {
+        $var = new Client;
+        $this->assertTrue(is_object($var));
+        unset($var);
+    }
+
+    /**
+    * Just check if the YourClass has no syntax error
+    *
+    * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+    * any typo before you even use this library in a real project.
+    *
+    */
+    public function test_empty_lookup_throws_exception()
+    {
+        $this->expectException(MissingArgException::class);
+        $var = new Client;
+        $this->assertTrue(is_object($var));
+        $var->lookup();
+        unset($var);
+    }
+}
