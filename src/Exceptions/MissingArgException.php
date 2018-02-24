f1a5f7f1367e08686bb72517a2cf5fa2881ed6d2
Dan <dpock32509@gmail.com>
2018-02-24 06:28:28 +0000

    Apply fixes from StyleCI

--- src/Exceptions/MissingArgException.php
+++ src/Exceptions/MissingArgException.php
@@ -5,11 +5,11 @@ class MissingArgException extends \Exception
 {
 
     // Redefine the exception so message isn't optional
-    public function __construct($message, $code = 0, Exception $previous = null) {
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
         // some code
 
         // make sure everything is assigned properly
         parent::__construct($message, $code, $previous);
     }
-
 }
