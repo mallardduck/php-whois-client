b53899138f5ad30d1e931159c56a23babe697c40
Dan Pock <dpock32509@gmail.com>
2018-05-13 17:32:41 -0400

    Merge BaseClient and Client classes into one...
    ...this seems to be the more logical solution for the time being.

--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,10 @@
 <?php
 namespace MallardDuck\Whois;
 
+use TrueBV\Punycode;
+use League\Uri\Components\Host;
+use MallardDuck\Whois\WhoisServerList\AbstractLocator;
+use MallardDuck\Whois\WhoisServerList\DomainLocator;
 use MallardDuck\Whois\Exceptions\MissingArgException;
 
 /**
@@ -12,15 +16,98 @@ use MallardDuck\Whois\Exceptions\MissingArgException;
  *
  * @version 1.0.0
  */
-class Client extends BaseClient
+class Client extends AbstractWhoisClient
 {
+
+    /**
+     * The TLD Whois locator class.
+     * @var AbstractLocator
+     */
+    protected $whoisLocator;
+
+    /**
+     * The Unicode for IDNA.
+     * @var \TrueBV\Punycode
+     */
+    protected $punycode;
+
+    /**
+     * The input domain provided by the user.
+     * @var string
+     */
+    public $inputDomain;
+
+    /**
+     * The parsed domain after validating and encoding.
+     * @var string
+     */
+    public $parsedDomain;
 
     /**
      * Construct the Whois Client Class.
      */
     public function __construct()
     {
-        parent::__construct();
+        $this->punycode = new Punycode();
+        $this->whoisLocator = new DomainLocator();
+    }
+
+    /**
+     * A unicode safe method for making whois requests.
+     *
+     * The main difference with this method is the benefit of punycode domains.
+     *
+     * @param  string $domain      The domain or IP being looked up.
+     * @param  string $whoisServer The whois server being queried.
+     *
+     * @return string              The raw results of the query response.
+     */
+    public function makeSafeWhoisRequest($domain, $whoisServer)
+    {
+        $this->parseWhoisDomain($domain);
+        // Form a socket connection to the whois server.
+        return $this->makeWhoisRequest($this->parsedDomain, $whoisServer);
+    }
+
+    /**
+     * Uses the League Uri Hosts component to get the search able hostname in PHP 5.6 and 7.
+     *
+     * @param string $domain The domain or IP being looked up.
+     *
+     * @return string
+     */
+    protected function getSearchableHostname($domain)
+    {
+        // Attempt to parse the domains Host component and get the registrable parts.
+        $host = new Host($domain);
+        return $host->getRegistrableDomain();
+    }
+
+    /**
+     * Takes the user provided domain and parses then encodes just the registerable domain.
+     * @param  string $domain The user provided domain.
+     *
+     * @return string Returns the parsed domain.
+     */
+    protected function parseWhoisDomain($domain)
+    {
+        if (empty($domain)) {
+            throw new MissingArgException("Must provide a domain name when using lookup method.");
+        }
+        $this->inputDomain = $domain;
+
+        // Check domain encoding
+        $encoding = mb_detect_encoding($domain);
+
+        $processedDomain = $this->getSearchableHostname($domain);
+
+        // Punycode the domain if it's Unicode
+        if ("UTF-8" === $encoding) {
+            $processedDomain = $this->punycode->encode($processedDomain);
+        }
+        $this->parsedDomain = $processedDomain;
+
+        return $processedDomain;
     }
 
     /**
