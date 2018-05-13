0d4d2bcb3e2fd2a8dcc36a75327d21f128d729a7
Dan Pock <dpock32509@gmail.com>
2018-05-13 17:50:17 -0400

    Update classes for structure changes

--- tests/WhoisClientTest.php
+++ tests/WhoisClientTest.php
@@ -2,7 +2,6 @@
 namespace MallardDuck\Whois\Test;
 
 use MallardDuck\Whois\Client;
-use PHPUnit\Framework\TestCase;
 use MallardDuck\Whois\Exceptions\MissingArgException;
 
 /**
@@ -13,7 +12,7 @@ use MallardDuck\Whois\Exceptions\MissingArgException;
 *
 * @author mallardduck <dpock32509@gmail.com>
 */
-class WhoisClientTest extends TestCase
+class WhoisClientTest extends BaseTest
 {
 
     /**
@@ -50,4 +49,75 @@ class WhoisClientTest extends TestCase
         $this->assertTrue(1 <= count(explode("\r\n", $results)));
         unset($var, $results);
     }
+
+    /**
+    * Test function comment stub.
+    */
+    public function testParseWhoisDomainFunction()
+    {
+        $client = new Client;
+        $this->assertTrue(method_exists($client, 'makeSafeWhoisRequest'));
+        $foo = self::getMethod($client, 'makeSafeWhoisRequest');
+        $foo->invokeArgs($client, ["danpock.me", "whois.nic.me"]);
+        unset($client, $foo);
+    }
+
+
+        /**
+         * Test function comment stub.
+         * @param string $domain Test domains!
+         * @param string $parsed Parsed domains!
+         * @dataProvider validDomainsProvider
+         */
+        public function testValidParsingDomains($domain, $parsed)
+        {
+            $client = new Client;
+            $this->assertTrue(method_exists($client, 'parseWhoisDomain'));
+            $foo = self::getMethod($client, 'parseWhoisDomain');
+            $wat = $foo->invokeArgs($client, [$domain]);
+            $this->assertTrue($parsed === $wat);
+            unset($client, $foo, $wat);
+        }
+
+        /**
+         * Test function comment stub.
+         */
+        public function validDomainsProvider()
+        {
+            return [
+                ['domain', ''],
+                ['sub.domain.wedding', 'domain.wedding'],
+                ['danpock.me.', 'danpock.me'],
+                ['www.sub.domain.xyz', 'domain.xyz'],
+                ['президент.рф', 'xn--d1abbgf6aiiy.xn--p1ai'],
+                ['xn--e1afmkfd.xn--80akhbyknj4f', 'xn--e1afmkfd.xn--80akhbyknj4f'],
+            ];
+        }
+
+        /**
+         * Test function comment stub.
+         * @param string $domain    Test domains!
+         * @param string $exception Exception class name!
+         * @dataProvider invalidDomainsProvider
+         */
+        public function testInvalidParsingDomains($domain, $exception)
+        {
+            $client = new Client;
+            $this->assertTrue(method_exists($client, 'parseWhoisDomain'));
+            $foo = self::getMethod($client, 'parseWhoisDomain');
+            $this->expectException($exception);
+            $foo->invokeArgs($client, [$domain]);
+            unset($client, $foo);
+        }
+
+        /**
+         * Test function comment stub.
+         */
+        public function invalidDomainsProvider()
+        {
+            return [
+                ['президент.рф', $this->getUriException()],
+                ['', MissingArgException::class],
+            ];
+        }
 }
