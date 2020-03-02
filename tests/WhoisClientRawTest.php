b871e652bbb73bc5c0f481cd284389ba63c2c0c6
MallardDuck <self@danpock.me>
2020-03-02 18:10:44 -0500

    Fix what class this is testing...
    ...I think I always intended this to be testing only features of AbstractClient accessible via SimpleClient. So no need to test full client here.

--- tests/WhoisClientRawTest.php
+++ tests/WhoisClientRawTest.php
@@ -2,7 +2,7 @@
 
 namespace MallardDuck\Whois\Test;
 
-use MallardDuck\Whois\Client;
+use MallardDuck\Whois\SimpleClient;
 
 /**
 *  Corresponding Class to test the whois Client class
@@ -20,7 +20,7 @@ class WhoisClientRawTest extends BaseTest
      */
     public function testIsThereAnySyntaxError()
     {
-        $var = new Client();
+        $var = new SimpleClient();
         $this->assertIsObject($var);
         unset($var);
     }
@@ -30,11 +30,53 @@ class WhoisClientRawTest extends BaseTest
      */
     public function testBasicRequestConcepts()
     {
-        $var = new Client();
+        $var = new SimpleClient();
         $this->assertIsObject($var);
         $var->createConnection("whois.nic.me");
         $status = $var->makeRequest("danpock.me");
         $response = $var->getResponseAndClose();
+        $containedResponse = strstr($response, "\r\n", true);
+        $this->assertSame("Domain Name: DANPOCK.ME", $containedResponse);
+
+        unset($response, $status, $var);
+    }
+
+    /**
+     * Basic test to check client syntax.
+     */
+    public function testConnectionDisconnects()
+    {
+
+        $reader = function & ($object, $property) {
+            $value = & \Closure::bind(function & () use ($property) {
+                return $this->$property;
+            }, $object, $object)->__invoke();
+
+            return $value;
+        };
+
+
+        $var = new SimpleClient();
+        $this->assertIsObject($var);
+        $var->createConnection("whois.nic.me");
+
+        // Grab a reference to the client and socket
+        $socketClient = & $reader($var, 'connection');
+        $socket = & $reader($socketClient, 'socket');
+
+        // Check our references once...
+        $this->assertIsObject($socketClient);
+        $this->assertIsResource($socket);
+
+        // Make the request and grab the data...
+        $status = $var->makeRequest("danpock.me");
+        $response = $var->getResponseAndClose();
+
+        // Check our refernces again...
+        $this->assertNull($socket);
+        $this->assertIsObject($socketClient);
+
+        // Check our response data...
         $containedResponse = strstr($response, "\r\n", true);
         $this->assertSame("Domain Name: DANPOCK.ME", $containedResponse);
 
